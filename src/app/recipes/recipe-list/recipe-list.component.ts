2a857207d83edd64fb8fa96b81f968aa96a17427
Gagan Chhabra <gagan.chhabra1990@gmail.com>
2021-04-25 16:58:00 +0530

    Added first static recipe into list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Manchurian recipe", "This is so delicious", "https://www.foodvedam.com/wp-content/uploads/2017/10/Veg-Manchurian.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
